a53047bdb387e258a53fe034ebfdbe19a0081548
MaxDMT16 <maxdmt1638@gmail.com>
2018-07-15 20:37:43 +0300

    make posts queries public update author in post

--- Fitness Guide (API)/Blog.Server/Database/Entities/CmsUser.cs
+++ Fitness Guide (API)/Blog.Server/Database/Entities/CmsUser.cs
@@ -1,9 +1,12 @@
-﻿namespace Blog.Server.Database.Entities
+﻿using System.Collections.Generic;
+
+namespace Blog.Server.Database.Entities
 {
     public class CmsUser : EntityBase
     {
         public string Email { get; set; }
         public string Password { get; set; }
         public string RefreshToken { get; set; }
+        public ICollection<Post> Posts { get; set; }
     }
 }
---
+++ Fitness Guide (API)/Blog.Server/Migrations/201807151724045_UpdateAuthorTypeInPosts.cs
@@ -0,0 +1,20 @@
+namespace Blog.Server.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class UpdateAuthorTypeInPosts : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Posts", "AuthorId", "dbo.Users");
+            AddForeignKey("dbo.Posts", "AuthorId", "dbo.CmsUsers", "Id", cascadeDelete: true);
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Posts", "AuthorId", "dbo.CmsUsers");
+            AddForeignKey("dbo.Posts", "AuthorId", "dbo.Users", "Id", cascadeDelete: true);
+        }
+    }
+}
